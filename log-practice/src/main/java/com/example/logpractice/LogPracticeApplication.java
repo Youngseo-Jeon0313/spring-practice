5255a7ed7cacfc186739d336a56aed78f30f496b
전영서 <20wjsdudtj@gmail.com>
2024-03-20 14:37:58 +0900

    log-practice 프로젝트 init

---
+++ log-practice/src/main/java/com/example/logpractice/LogPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.logpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogPracticeApplication.class, args);
+    }
+
+}
---
+++ log-practice/src/test/java/com/example/logpractice/LogPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
