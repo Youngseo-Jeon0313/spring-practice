778fffe5e287dc9a60550d9eca617467ce390739
전영서 <20wjsdudtj@gmail.com>
2024-03-20 15:25:29 +0900

    Provider를 사용하여 Dependency Lookup으로 request 스코프 요청 초기화 오류 해결 Dependency Lookup을 사용하여 빈 주입 시점에 주입받는다.

--- log-practice/src/main/java/com/example/logpractice/controller/LogController.java
+++ log-practice/src/main/java/com/example/logpractice/controller/LogController.java
@@ -4,6 +4,7 @@ import com.example.logpractice.common.MyLogger;
 import com.example.logpractice.service.LogService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,13 +13,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogController {
     private final LogService logService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
+
         myLogger.log("controller test");
         logService.logic("testId");
         return "OK";
--- log-practice/src/main/java/com/example/logpractice/service/LogService.java
+++ log-practice/src/main/java/com/example/logpractice/service/LogService.java
@@ -3,13 +3,15 @@ package com.example.logpractice.service;
 
 import com.example.logpractice.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogService {
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
     public void logic(String id){
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = "+id);
     }
 }
