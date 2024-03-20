7eedb2140abe246688dc645510c0490c1677330b
전영서 <20wjsdudtj@gmail.com>
2024-03-20 15:31:40 +0900

    프록시 사용하여 해결 우선 가짜 프록시 객체를 요청한 후 요청이 올 때 진짜 빈을 요청하는 위임 로직 사용

--- log-practice/src/main/java/com/example/logpractice/controller/LogController.java
+++ log-practice/src/main/java/com/example/logpractice/controller/LogController.java
@@ -4,7 +4,6 @@ import com.example.logpractice.common.MyLogger;
 import com.example.logpractice.service.LogService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,15 +12,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogController {
     private final LogService logService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
-
         myLogger.log("controller test");
         logService.logic("testId");
         return "OK";
--- log-practice/src/main/java/com/example/logpractice/service/LogService.java
+++ log-practice/src/main/java/com/example/logpractice/service/LogService.java
@@ -3,15 +3,13 @@ package com.example.logpractice.service;
 
 import com.example.logpractice.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogService {
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
     public void logic(String id){
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = "+id);
     }
 }
